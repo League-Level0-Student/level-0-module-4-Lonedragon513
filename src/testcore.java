236cb0daa96f4e50b3edb10c3f88c40c139590a4
theonak4 <league@wts14-4.attlocal.net>
2018-04-21 17:27:53 -0700

    pi; testscore; happy pet; ownadd

--- src/testcore.java
+++ src/testcore.java
@@ -1,20 +1,18 @@
 import javax.swing.JOptionPane;
 
 public class testcore {
-public static void main(String[] args) {
-	String S = JOptionPane.showInputDialog("what is your test score");
-	Double Q = Double.parseDouble(S);
-	if (Q == 100) {
-		JOptionPane.showMessageDialog(null, "good job");
-	}
-	else if (Q >= 70) {
-		JOptionPane.showMessageDialog(null, "you did good");
+	public static void main(String[] args) {
+		String S = JOptionPane.showInputDialog("what is your test score");
+		Double Q = Double.parseDouble(S);
+		if (Q == 100) {
+			JOptionPane.showMessageDialog(null, "good job");
+		} else if (Q >= 70) {
+			JOptionPane.showMessageDialog(null, "you did good");
 
-	}
-	else if (Q = 70) {
-		JOptionPane.showMessageDialog(null, "you need to study");
+		} else if (Q <= 70) {
+			JOptionPane.showMessageDialog(null, "you need to study");
 
+		}
 	}
-}
 
 }
